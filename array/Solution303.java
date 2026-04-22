2a08d4c2e20d51bb860dced4c9ab876a6a8a5567
PritamMugal <mugal.pritam@gmail.com>
2026-04-22 21:24:47 +0530

    Solution303

---
+++ array/Solution303.java
@@ -0,0 +1,23 @@
+package array;
+
+public class Solution303 {
+    private int[] nums;
+
+    public Solution303(int[] nums) {
+        this.nums = nums;
+    }
+
+    public int sumRange(int left, int right) {
+        int[] prefix = new int[nums.length+1];
+        for(int i = 0; i < nums.length; i++) {
+            prefix[i+1] = prefix[i] + nums[i];
+        }
+        return prefix[right+1] + prefix[left];
+    }
+    public static void main(String[] args) {
+        int[] nums = {-2, 0, 3, -5, 2, -1};
+        Solution303 sol = new Solution303(nums);
+        int sum = sol.sumRange(0,2);
+        System.out.println(sum);
+    }
+}
